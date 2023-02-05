784d8da9bae369d354365bb3cc42364dbedc5c9e
hassan-ish <hassan.abuishaiba@gmail.com>
2023-02-05 09:12:35 +0200

    useable Container

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Containerr} from  './style'
+
+const Container = (props) => {
+
+  return <Containerr>{props.children}</Containerr>;
+};
+
+export default Container;
---
+++ src/Components/Container/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Containerr = styled.div`
+  padding: 0 130px;
+`;
