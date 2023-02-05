f68c74579a092de5ac34847a7a1434c2fc8a17ee
hassan-ish <hassan.abuishaiba@gmail.com>
2023-02-05 09:19:30 +0200

    Sections Title

---
+++ src/Components/Title/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TitleWrapper } from "./style";
+
+const Title = (props) => {
+  return <TitleWrapper>{props.children}</TitleWrapper>;
+};
+
+export default Title;
---
+++ src/Components/Title/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TitleWrapper = styled.div`
+  font-weight: 600;
+  font-size: 24px;
+  color: #1c1c1c;
+  margin-bottom: 24px;
+`;
